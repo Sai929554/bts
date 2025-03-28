71c099022f152af8189b8385b7199c19ac586836
Sai929554 <shivarathrisaikumar991@gmail.com>
2025-03-28 09:15:41 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -508,4 +508,3 @@ def download_file(filename):
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
